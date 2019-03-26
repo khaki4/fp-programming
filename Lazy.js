bd7895dd71079fbc9c6017e424649ee29727f595
pi3rrot <4dakto@gmail.com>
2019-03-26 21:18:28 +0900

    update

--- Lazy.js
+++ Lazy.js
@@ -42,9 +42,13 @@ L.map = curry(function* (f, iter) {
   }
 });
 
+const nop = Symbol('nop');
+
 L.filter = curry(function* (f, iter) {
   for (const a of iter) {
-    if (f(a)) yield a;
+    const b = go1(a, f);
+    if (b instanceof Promise) yield b.then(b => b ? a : Promise.reject(nop));
+    else if (b) yield b;
   }
 });
 
@@ -62,7 +66,8 @@ const take = curry((limit, iter) => {
     while (!(cur = iter.next()).done) {
       const a = cur.value;
       if (a instanceof Promise) return a.then(a =>
-        (res.push(a), res).length === limit ? res : recur());
+        (res.push(a), res).length === limit ? res : recur()
+      ).catch(e => e === nop ? recur() : Promise.reject(e));
       res.push(a);
       if (res.length === limit) return res;
     }
@@ -91,18 +96,22 @@ const filter =  curry(pipe(L.filter, takeAll));
 const find = curry((f, iter) => go(iter, L.filter(f), take(1), ([a]) => a));
 
 // 접기 reduce
+const reduceF = (acc, a, f) =>
+  a instanceof Promise ?
+    a.then(a => f(acc, a), e => e === nop ? acc : Promise.reject(e)) :
+    f(acc, a);
+
+const head = iter => go1(take(1, iter), ([h]) => h);
+
 const reduce = curry((f, acc, iter) => {
-  if (!iter) {
-    iter = acc[Symbol.iterator]();
-    acc = iter.next().value;
-  } else {
-    iter = iter[Symbol.iterator]();
-  }
+  if (!iter) return reduce(f, head(iter = acc[Symbol.iterator]()), iter);
+
+  iter = iter[Symbol.iterator]();
+
   return go1(acc, function recur(acc) {
     let cur;
     while (!(cur = iter.next()).done) {
-      const a = cur.value;
-      acc = f(acc, a);
+      acc = reduceF(acc, cur.value, f);
       if (acc instanceof Promise) return acc.then(recur)
     }
     return acc;
@@ -209,8 +218,24 @@ const fg = id => Promise.resolve(id).then(g).then(f).catch(a => a);
 //   a => a + 10000,
 //   log).catch(a => console.log(a));
 
-go(
-  range(3),
-  L.map(a => Promise.resolve(a + 10)),
-  takeAll,
+// go(
+//   range(3),
+//   L.map(a => Promise.resolve(a + 10)),
+//   takeAll,
+//   log);
+
+// go([1,2,3,4,5,6],
+//   L.map(a => Promise.resolve(a * a)),
+//   L.filter(a => {
+//     return a % 2;
+//   }),
+//   take(2),
+//   log);
+
+const add = (a, b) => a + b;
+
+go([1, 2, 3, 4, 5],
+  L.map(a => Promise.resolve(a * a)),
+  L.filter(a => Promise.resolve(a * 2)),
+  reduce(add),
   log);
